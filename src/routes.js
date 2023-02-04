ba1d9abef011333cbf015cded230718aabcbb828
Edwin Encomienda <edwinencomienda@Edwin-Encomienda-MBPM1.local>
2023-02-04 16:02:41 +0800

    fixes

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,25 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory } from 'vue-router'
 
 // import all files in the pages directory
-const pages = import.meta.globEager("./pages/*.vue")
+const pages = import.meta.globEager('./pages/*.vue')
 const routePages = []
 for (const path in pages) {
-    const name = path.split("/").pop().split(".")[0]
-    routePages.push({
-        title: name,
-        path: `/${name}`,
-        component: pages[path].default,
-    })
+  const name = path.split('/').pop().split('.')[0]
+  routePages.push({
+    title: name,
+    path: `/${name}`,
+    component: pages[path].default,
+  })
 }
 
 const routes = [
-    { title: "Home", path: "/", component: () => import("./pages/index.vue") },
-    ...routePages,
+  { title: 'Home', path: '/', component: () => import('./pages/index.vue') },
+  ...routePages,
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 })
 
 export { routes }
