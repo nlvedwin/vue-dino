d1135142475bf27e37c8d2b95f9ada5b7fb7d8d2
Edwin Encomienda <edwinencomienda@Edwin-Encomienda-MBPM1.local>
2023-02-04 16:00:17 +0800

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,46 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import { resolve } from "path"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// get mode from command line
+const mode = process.env.Site
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        port: 8200,
+  server: {
+    port: 8200,
+  },
+  plugins: [vue()],
+  define: {
+    'process.env': {
+      VUE_APP_VERSION: JSON.stringify(require('./package.json').version),
     },
-    plugins: [vue()],
-    define: {
-        "process.env": {
-            VUE_APP_VERSION: JSON.stringify(require("./package.json").version),
-        },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
     },
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "./src"),
-        },
-    },
-    optimizeDeps: {
-        include: ["choices.js", "choices/public/assets/styles/choices.min.css"],
-    },
-    build: {
-        lib: {
-            entry: resolve(__dirname, "src/index.js"),
-            name: "VueDino",
-        },
-        rollupOptions: {
-            external: ["vue"],
+  },
+  optimizeDeps: {
+    include: ['choices.js', 'choices/public/assets/styles/choices.min.css'],
+  },
+  build:
+    process.env.DOCS === 'true'
+      ? {}
+      : {
+          lib: {
+            entry: resolve(__dirname, 'src/index.js'),
+            name: 'VueDino',
+          },
+          rollupOptions: {
+            external: ['vue'],
             output: {
-                // Provide global variables to use in the UMD build
-                // Add external deps here
-                globals: {
-                    vue: "Vue",
-                },
+              // Provide global variables to use in the UMD build
+              // Add external deps here
+              globals: {
+                vue: 'Vue',
+              },
             },
+          },
         },
-    },
 })
